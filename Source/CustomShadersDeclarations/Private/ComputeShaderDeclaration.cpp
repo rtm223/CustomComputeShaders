f39425a8dab8be06979ab0cc4ca69b87a764031c
Richard Meredith <rtm223@hotmail.com>
2021-12-15 11:27:44 +0100

    Attempt to force onto compute shader - unclear id this actually works

--- Source/CustomShadersDeclarations/Private/ComputeShaderDeclaration.cpp
+++ Source/CustomShadersDeclarations/Private/ComputeShaderDeclaration.cpp
@@ -131,10 +131,10 @@ void FWhiteNoiseCSManager::Execute_RenderThread(FRHICommandListImmediate& RHICmd
 		ComputeShaderOutputDesc.DebugName = TEXT("WhiteNoiseCS_Output_RenderTarget");
 		GRenderTargetPool.FindFreeElement(RHICmdList, ComputeShaderOutputDesc, ComputeShaderOutput, TEXT("WhiteNoiseCS_Output_RenderTarget"));
 	}
-	
-	// Specify the resource transition - [RTM note] prior to 4.27 this specified Compute, but was actually ignored under the hood in 4.27. There is an EHRIPipeline::AsycCompute flag, though I'm unsure how we set it
-	// TODO - investigate ERHIPipeline::AsyncCompute
-	RHICmdList.Transition(FRHITransitionInfo(ComputeShaderOutput->GetRenderTargetItem().UAV, ERHIAccess::None, ERHIAccess::ERWBarrier, EResourceTransitionFlags::None));
+
+	// Specify the resource transition
+	FRHITransitionInfo transitionInfo(ComputeShaderOutput->GetRenderTargetItem().UAV, ERHIAccess::UAVCompute, ERHIAccess::UAVCompute, EResourceTransitionFlags::None);
+	RHICmdList.Transition(transitionInfo);
 
 	//Fill the shader parameters structure with tha cached data supplied by the client
 	FWhiteNoiseCS::FParameters PassParameters;
